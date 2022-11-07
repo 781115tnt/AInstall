62a98ee11888dc75c727fff4de2c233be5592f55
781115tnt <71996697+781115tnt@users.noreply.github.com>
2022-11-07 12:37:51 +0700

    subvolume /home

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -44,8 +44,8 @@ sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 # create partitions
 sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
 sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::+30G --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, 30GB
-sgdisk -n 4::+16G --typecode=4:8200 --change-name=4:'SWAP' ${DISK} # partition 4 (Swap), default start, 16GB
+sgdisk -n 3::+12G --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, 30GB
+sgdisk -n 4::+2G --typecode=4:8200 --change-name=4:'SWAP' ${DISK} # partition 4 (Swap), default start, 16GB
 sgdisk -n 5::-0 --typecode=5:8300 --change-name=5:'HOME' ${DISK} # partition 5 (Home), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
@@ -61,6 +61,7 @@ echo -ne "
 # @description Creates the btrfs subvolumes. 
 createsubvolumes () {
     btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/home/@
     btrfs subvolume create /mnt/@var
     btrfs subvolume create /mnt/@tmp
     btrfs subvolume create /mnt/@.snapshots
@@ -71,6 +72,7 @@ mountallsubvol () {
     mount -o ${MOUNT_OPTIONS},subvol=@tmp ${partition3} /mnt/tmp
     mount -o ${MOUNT_OPTIONS},subvol=@var ${partition3} /mnt/var
     mount -o ${MOUNT_OPTIONS},subvol=@.snapshots ${partition3} /mnt/.snapshots
+    mount -o ${MOUNT_OPTIONS},subvol=@ ${partition5} /mnt/home
 }
 
 # @description BTRFS subvolulme creation and mounting. 
@@ -83,6 +85,7 @@ subvolumesetup () {
     mount -o ${MOUNT_OPTIONS},subvol=@ ${partition3} /mnt
 # make directories home, .snapshots, var, tmp
     mkdir -p /mnt/{home,var,tmp,.snapshots}
+    mount -o ${MOUNT_OPTIONS},subvol=@ ${partition5} /mnt/home
 # mount subvolumes
     mountallsubvol
 }
@@ -105,9 +108,9 @@ if [[ "${FS}" == "btrfs" ]]; then
     mkswap -c ${partition4}
     mkfs.btrfs -L HOME ${partition5} -f
     mount -t btrfs ${partition3} /mnt
+    mount -t btrfs ${partition5} /mnt/home
     swapon ${partition4}
     subvolumesetup
-    mount -o ${MOUNT_OPTIONS},subvol=@ ${partition5} /mnt/home
 elif [[ "${FS}" == "ext4" ]]; then
     mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
     mkfs.ext4 -L ROOT ${partition3}
@@ -128,9 +131,9 @@ elif [[ "${FS}" == "luks" ]]; then
     mkfs.btrfs -L HOME ${partition5}
 # create subvolumes for btrfs
     mount -t btrfs ${partition3} /mnt
+    mount -t btrfs ${partition5} /mnt/home
     swapon ${partition4}
     subvolumesetup
-    mount -o ${MOUNT_OPTIONS},subvol=@ ${partition5} /mnt/home
 # store uuid of encrypted partition for grub
     echo ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value ${partition3}) >> $CONFIGS_DIR/setup.conf
 fi
