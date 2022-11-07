87cdf77340bfcf4e602dfc4c9dfe2bfb06d51884
781115tnt <71996697+781115tnt@users.noreply.github.com>
2022-11-07 11:25:56 +0700

    Update 0-preinstall.sh

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -46,7 +46,7 @@ sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
 sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
 sgdisk -n 3::+30G --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, 30GB
-sgdisk -n 4::+4G --typecode=4:8200 --change-name=4:'SWAP' ${DISK} # partition 4 (Swap), default start, 4GB
+sgdisk -n 4::+16G --typecode=4:8200 --change-name=4:'SWAP' ${DISK} # partition 4 (Swap), default start, 16GB
 sgdisk -n 5::-0 --typecode=5:8300 --change-name=5:'HOME' ${DISK} # partition 5 (Home), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
